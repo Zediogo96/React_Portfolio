cd09d971157b792c8ed3f8239a0da572e53632c9
Zé Diogo <josediogo96@outlook.com>
2023-07-10 00:42:19 +0100

    Landing page almost complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
-import { useState, Dispatch, SetStateAction } from "react";
+import { useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import DotGroup from "./components/DotGroup";
+import { motion } from "framer-motion";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState('home');
-  const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)');
+	const [selectedPage, setSelectedPage] = useState("home");
+	const isDesktop = useMediaQuery("(min-width: 1060px)");
 
-  return (
-    <div className="app bg-deep-blue">
-      <Navbar 
-        selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage}
-      />
-    </div>
-  );
+	return (
+		<div className="app bg-deep-blue">
+			<motion.div
+					initial="hidden"
+					whileInView="visible"
+					viewport={{ once: true, amount: 0.5 }}
+					transition={{ duration: 0.5 }}
+					variants={{
+						hidden: { opacity: 0, x: 100 },
+						visible: { opacity: 1, x: 0 },
+					}}
+				>
+				<Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+			</motion.div>
+			<div className="w-5/6 mx-auto md:h-full">
+				{isDesktop && (
+					<DotGroup
+						selectedPage={selectedPage}
+						setSelectedPage={setSelectedPage}
+					/>
+				)}
+				<motion.div
+					margin="0 0 -200px 0"
+					amount="all"
+					onViewportEnter={() => setSelectedPage("home")}
+				>
+					<Landing setSelectedPage={setSelectedPage} />
+				</motion.div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -36,7 +36,7 @@ function Navbar({ selectedPage, setSelectedPage }) {
 	return (
 		<nav className={`z-40 w-full fixed top-0 py-6`}>
 			<div className="flex items-center justify-between mx-auto w-5/6">
-				<h4 className="font-playfair text-3xl font-bold text-white"> JD </h4>
+				<h4 className="font-playfair text-3xl font-bold text-white"> ZD </h4>
 				{/* DESKTOP NAV */}
 				{isAboveSmallScreens ? (
 					<div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
